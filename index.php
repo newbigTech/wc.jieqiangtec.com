cba3e7f8b6deffd7fb6115ff06b65b12efea7f1c
jieqiang <1569501393@qq.com>
2017-09-03 10:56:13 +0800

    test

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 // var_dump('TODO jieqiangtest 111');exit;
 require './framework/bootstrap.inc.php';
+
 $host = $_SERVER['HTTP_HOST'];
 if (!empty($host)) {
 	$bindhost = pdo_fetch("SELECT * FROM ".tablename('site_multi')." WHERE bindhost = :bindhost", array(':bindhost' => $host));
